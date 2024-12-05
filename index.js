c952a8bc034c92a64e82a39ac2a732779adc405e
Sharad1013 <sharadsinha8789@gmail.com>
2024-12-05 23:15:55 +0530

    added login/register routes and jwt tokenization

--- index.js
+++ index.js
@@ -2,17 +2,26 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
+const userRoute = require("./routes/user");
 const dotenv = require("dotenv");
 dotenv.config();
+
 const PORT = process.env.PORT || 3000;
+
 const path = require("path");
+const bodyParser = require("body-parser");
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 // home route
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "try.html"));
 });
+
+//userRoute
+app.use("/api/user", userRoute);
 
 // listening on PORT
 app.listen(PORT, () => {
---
+++ routes/user.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../schema/user.schema");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+//register route
+router.post("/register", async (req, res) => {
+  const { name, email, password, mobile } = req.body;
+
+  const isUserExist = await User.findOne({ email });
+  if (isUserExist) {
+    return res.status(400).json({
+      message: "User already exist",
+    });
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  try {
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      mobile,
+    });
+    res.status(200).json({
+      message: "User Created",
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error in creating user" });
+  }
+});
+
+//login route
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).json({ message: "Wrong username or password" });
+  }
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ message: "Wrong username or password" });
+  }
+  const payload = {
+    id: user._id,
+  };
+  // Remember you didn't add the JWT_SECRET on the deployment platform (render)
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+  res.status(200).json({ token });
+});
+module.exports = router;
